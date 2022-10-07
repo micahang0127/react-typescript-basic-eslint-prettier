62f13a8b6eabc7a54619c4341faad5c56d3a50d0
sanga.park <sagna.park@epikar.com>
2022-10-07 15:15:28 +0900

    tsx React.FC type으로 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App';
+import Greetings from './Greetings';
 
-// function App() {
 const App: React.FC = () => {
-  // React.FC 타입으로 지졍 -> 장단점이 있음
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editttttsssssttt <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const onClick = (name: string) => {
+    console.log(`${name} says hello`);
+  };
+  return <Greetings name="Hello" onClick={onClick} />;
 };
 
 export default App;
